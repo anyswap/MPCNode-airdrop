4dee9fe176c94140757c881b96d08a1910f34986
jowenshaw <jowenshaw@gmail.com>
2020-12-11 11:56:11 +0800

    claim reward from contract's owner

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -6,8 +6,10 @@ pragma experimental ABIEncoderV2;
 interface IERC20 {
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
+// Airdrop: reward is from the owner of this contract
 contract Airdrop {
     IERC20 public token;
     address payable public owner;
@@ -54,19 +56,11 @@ contract Airdrop {
     function claim() public returns (bool) {
         uint256 amount = rewards[msg.sender];
         require(amount > 1, "invalid amount");
-        uint256 balance = token.balanceOf(address(this));
+        uint256 balance = token.balanceOf(address(owner));
         require(balance >= amount, "not enough balance");
+        assert(token.transferFrom(owner, msg.sender, amount));
         rewards[msg.sender] = 1;
-        assert(token.transfer(msg.sender, amount));
         emit Claim(msg.sender, amount);
         return true;
-    }
-
-    function destroy() public onlyOwner {
-        uint256 balance = token.balanceOf(address(this));
-        if (balance > 0) {
-            token.transfer(owner, balance);
-        }
-        selfdestruct(owner);
     }
 }
